c46852cbd637b46db33f815e5e8b618a655774f5
kolarikjan <58089918+kolarikjan@users.noreply.github.com>
2021-12-26 20:46:42 +0100

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".homepage-carousel").owlCarousel({
+        items : 1, 
+        dots: false,
+        nav: false,
+        stagePadding: 0,
+        margin:0,
+        singleItem:true
+    });
+});
